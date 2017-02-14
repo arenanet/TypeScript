b98fba1d1407f57da748a804b5a9d29891b9af88
Brett Vickers <brett@beevik.com>
2017-02-13 22:22:32 -0800

    Add --preserveFunctionNames command-line option.
    This option causes class methods to have their names preserved in
    generated JavaScript.

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -3370,6 +3370,7 @@ namespace ts {
         paths?: MapLike<string[]>;
         /*@internal*/ plugins?: PluginImport[];
         preserveConstEnums?: boolean;
+        preserveFunctionNames?: boolean;
         project?: string;
         /* @internal */ pretty?: DiagnosticStyle;
         reactNamespace?: string;
