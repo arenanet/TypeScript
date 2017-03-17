21df935abf1bb5ff2747781298effdc53726f4d2
Brett Vickers <brett@arena.net>
2017-03-17 09:54:42 -0700

    Add --moduleBase command-line option.
    With the --moduleBase option, a user can change the interpretation
    of absolute paths that begin with a single '/' in a triple-slash
    reference path or include() path.
    
    If the --moduleBase option is used, absolute paths are prepended
    with the path provided as an option to --moduleBase.
    
    For example, when run with "--moduleBase /foo/bar", the following
    reference path:
    
       ///<reference path="/quux/test.ts"/>
    
    becomes "/foo/bar/quux/test.ts".

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -3350,6 +3350,7 @@ namespace ts {
         maxNodeModuleJsDepth?: number;
         module?: ModuleKind;
         moduleResolution?: ModuleResolutionKind;
+        moduleBase?: string;
         newLine?: NewLineKind;
         noEmit?: boolean;
         /*@internal*/noEmitForJsFiles?: boolean;
